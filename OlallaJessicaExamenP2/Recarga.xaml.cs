eca2a3a09269c732d01cd83ef36a4111e554d8e3
Wikiniki15 <wiki.niki.olalla@gmail.com>
2024-12-02 09:00:00 -0500

    Recarga

--- OlallaJessicaExamenP2/Recarga.xaml.cs
+++ OlallaJessicaExamenP2/Recarga.xaml.cs
@@ -1,20 +1,40 @@
 using Microsoft.Maui.Controls;
+using OlallaJessicaExamenP2.Interfaces;
 using System.Reflection;
 
 namespace OlallaJessicaExamenP2;
 
 public partial class Recarga : ContentPage
 {
-	public Recarga()
+    IRecargaRepository _recargaRepository;
+    Recarga recarga = new Recarga();
+    public Recarga()
 	{
-		InitializeComponent();
-	}
+        _recargaRepository = new RecargaRepository();
+        InitializeComponent();
 
-    /*private void RecargaClicked(object sender, EventArgs e)
+        recarga = _recargaRepository.DevuelveEstudianteUDLA();
+        BindingContext = recarga;
+    }
+
+    private async void RecargaClicked(object sender, EventArgs e)
     {
-        if (BindingContext is Models.Recarga note)
-            File.WriteAllText(note.Filename, TextEditor.Text);
+        Recarga estudiante = new Recarga
+        {
+            Nombre = editor_nombre.Text,
+            Numero = editor_numero.Text,
+        };
+        bool guardar_recarga = _recargaRepository.CrearRecarga(recarga);
 
-        await Shell.Current.GoToAsync("..");
-    }*/
+        if (guardar_recarga)
+        {
+            await DisplayAlert("Alert", "Todo posi", "OK");
+            Navigation.PushAsync(new Recarga());
+        }
+        else
+        {
+            await DisplayAlert("Alert", "Negado mi pana", "OK");
+        }
+    }
+    
 }
