ac353559d7c40ab6951f2f8eac9a84b9104efc7f
Wikiniki15 <wiki.niki.olalla@gmail.com>
2024-12-02 07:30:41 -0500

    Tab

---
+++ OlallaJessicaExamenP2/Grid.xaml.cs
@@ -0,0 +1,9 @@
+namespace OlallaJessicaExamenP2;
+
+public partial class Grid : ContentPage
+{
+	public Grid()
+	{
+		InitializeComponent();
+	}
+}
---
+++ OlallaJessicaExamenP2/Recarga.xaml.cs
@@ -0,0 +1,9 @@
+namespace OlallaJessicaExamenP2;
+
+public partial class Recarga : ContentPage
+{
+	public Recarga()
+	{
+		InitializeComponent();
+	}
+}
